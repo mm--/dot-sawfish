62c70652b972b788cabb032cccf94773f3b5191f
Joshua Moller-Mara <j.moller-mara@berkeley.edu>
2014-08-07 01:26:39 -0700

    Time tracking script, prevent exit on pipe failure

--- scripts/computer-time-counter.sh
+++ scripts/computer-time-counter.sh
@@ -2,11 +2,11 @@
 # This is a a small script made to track and limit how much time I
 # spend on my computer each day. If I exceed a threshold for the day, the computer goes to sleep.
 POLLTIME=25
-if [ -n "$1" ]; then
+if [ -n "$2" ]; then
     POLLTIME=$1
 fi
 COUNTER=0
-if [ -n "$2" ]; then
+if [ -n "$1" ]; then
     COUNTER=$(($1 * 60))
 fi
 THRESHHOURS=5
@@ -17,6 +17,8 @@ function getdate {
 }
 
 DATE=`getdate`
+LOCKED="FALSE"
+SAVEDTIME=`date +"%Y-%m-%d %H:%M"`
 
 while :
 do
@@ -24,15 +26,16 @@ do
        pgrep xtrlock
     then
 	echo "Screen locked"
+	LOCKED="TRUE"
     else
 	NEWDATE=`getdate`
+	LOCKED="FALSE"
 	if [ "$NEWDATE" == "$DATE" ]
 	then
 	    COUNTER=$((COUNTER + POLLTIME))
 	else
 	    echo "Resetting counter"
 	    echo "Write to CSV"
-	    echo "\"$DATE\",$COUNTER" >> ~/.sawfish/data/computertime.csv
 	    COUNTER=0
 	    DATE=`getdate`
 	fi
@@ -43,11 +46,19 @@ do
 	    sleep 300		# If we re-awake, give 5 minutes time?
 	fi
     fi
-    echo "COMPUTERTIME:$COUNTER" | tee -a ~/.sawfish/scripts/myfifo
+    
     UNIXTIME=`date +"%s"`
-    echo "COMPUTERVIRTUALSTART:"$((UNIXTIME - COUNTER)) | tee -a ~/.sawfish/scripts/myfifo
+    THETIME=`date +"%Y-%m-%d %H:%M"`
+    if [ "$SAVEDTIME" != "$THETIME" ]
+    then
+	SAVEDTIME="$THETIME"
+	echo "\"$DATE\"","\"$THETIME\",$UNIXTIME,$COUNTER,$LOCKED,$POLLTIME" >> ~/.sawfish/data/computertime.csv
+    fi
+
+    echo "TIME IS $THETIME"
+    echo $((UNIXTIME - COUNTER)) > ~/.sawfish/data/computer_virtual_start.txt
+    echo "COMPUTERTIME:$COUNTER"
     echo "COMPUTERTIMELEFT:"$((THRESHOLD - COUNTER)) | tee -a ~/.sawfish/scripts/myfifo
-    echo "UPDATE: please" >> ~/.sawfish/scripts/myfifo
     sleep $POLLTIME
 done
 
