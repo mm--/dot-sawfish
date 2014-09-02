c4b623a627ceff437258ae80f3eda0bd42abdb39
Joshua Moller-Mara <j.moller-mara@berkeley.edu>
2014-09-01 17:26:12 -0700

    Use ceftoolbar-gtk by default

--- scripts/startup-scripts.sh
+++ scripts/startup-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ~/.sawfish/scripts/dzen_multiplexer.py | dzen2 -x 63 -w 1400 -h 16 -ta 'l' -fn 'fixed:pixelsize=10' -title-name dzenRIGHT -dock -fn 'terminus' -e '' &
-~/.sawfish/ceftoolbar/ceftoolbar.py &
+~/.sawfish/ceftoolbar/ceftoolbar-gtk.py &
 stalonetray -bg "black" --geometry 10x1-0+0 --grow-gravity W --icon-gravity NE --icon-size 16 --dockapp-mode simple &
 # conky -c ~/.sawfish/conky/conky_cpu.conf &
 fvwm-root -r ~/.fvwm/wallpaper/bridge.png &
