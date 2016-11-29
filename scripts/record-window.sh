5cd05fb6af7e24c340e4268a923ff9e26389fd20
Josh Moller-Mara <j.mollermara@nyu.edu>
2016-11-29 12:42:37 +0800

    Better interface for recording videos

--- scripts/record-window.sh
+++ scripts/record-window.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-if pgrep -u `whoami` -f "recordmydesktop"
+function pgrep_live {
+  pids=$(pgrep -u `whoami` -f "$1");
+  [ "$pids" ] || return;
+  ps -o s= -o pid= $pids | sed -n 's/^[^ZT][[:space:]]\+//p';
+}
+
+if [ $(pgrep_live "recordmydesktop" | wc -l) -gt 0 ]
 then
+    echo "Stopping session"
     pkill --signal SIGINT -u `whoami` -f "recordmydesktop"
+    xterm -e "tmux attach -t recorddesktop" &
 else
-    recordmydesktop --windowid $1 --device pulse -o $2 &
+    echo "Starting new session"
+    tmux new-session -d -s "recorddesktop" "recordmydesktop --device default --windowid $1 -o $2"
+    # recordmydesktop --device default --windowid $1 -o $2
 fi
 
