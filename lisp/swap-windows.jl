4a1e0941ef663546aaffa3faee23ce4d94e7a9fa
Josh Moller-Mara <j.mollermara@nyu.edu>
2017-08-13 08:41:49 +0800

    Maintain same stacking order when swapping

--- lisp/swap-windows.jl
+++ lisp/swap-windows.jl
@@ -9,11 +9,23 @@
     (let* ((rects (rectangles-from-windows (list josh-swap-windows-start-win focusedwin) identity))
 	   (start-rect (car rects))
 	   (end-rect (cadr rects)))
+      (josh-swap-stack josh-swap-windows-start-win focusedwin)
+      (setq josh-swap-windows-start-win nil)
       (true-move (nth 4 start-rect) end-rect)
-      (true-move (nth 4 end-rect) start-rect)
-      (setq josh-swap-windows-start-win nil))))
+      (true-move (nth 4 end-rect) start-rect))))
+
+(define (josh-swap-stack win1 win2)
+  (restack-windows (mapcar (lambda (y)
+      				 (cond
+      				  ((equal y win1) win2)
+      				  ((equal y win2) win1)
+      				  (t y)))
+      			     (let ((vwf (viewport-windows-filtered)))
+      			       (filter (lambda (x) (member x vwf)) (mapped-stacking-order))))))
 
 ;; (remove-hook 'focus-in-hook josh-sw-focus-swap)
+;; (setq focus-in-hook (cdr focus-in-hook))
+;; focus-in-hook
 (add-hook 'focus-in-hook josh-sw-focus-swap)
 
 (define (josh-swap-window-interactively w)
