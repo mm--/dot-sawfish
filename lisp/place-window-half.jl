3dc6c4daf9c4b061a1ca962c22a00bb80db65f3a
Josh Moller-Mara <j.mollermara@nyu.edu>
2018-01-19 10:18:40 +0800

    Set up some emacs key->window bindings automatically

--- lisp/place-window-half.jl
+++ lisp/place-window-half.jl
@@ -24,11 +24,37 @@
 
 (define-placement-mode 'full-or-half-viewport place-window-full-or-half #:for-normal t)
 
+(require 'sawfish.wm.frames)
+(define (josh-set-emacs-win win binding style)
+  (set-frame-style win style)
+  (true-move win (shrink-rect (subd (calculate-workarea) 'left) 20))
+  (josh-bind-window binding win))
+
+(setq josh-emacs-bind-styles
+      '(("3" . candido)
+	("2" . absolute-e)
+	("4" . Elberg-tabbed)
+	("5" . microGUI)))
+
+(define (place-emacs-half win)
+  "Place emacs in the half-viewport position. Also make sure we
+set the frame correctly if some window-key bindings don't exist."
+  (let* ((unbound-emacs (car (filter (lambda (binding)
+				       (let ((thewin (cdr (assoc 'window (assoc binding josh-window-alist)))))
+					 (not (when thewin (window-mapped-p thewin)))))
+				   (mapcar car josh-emacs-bind-styles))))
+       (unbound-style (cdr (assoc unbound-emacs josh-emacs-bind-styles))))
+  (if unbound-emacs
+      (josh-set-emacs-win win unbound-emacs unbound-style)
+    (place-window-half win))))
+
+(define-placement-mode 'emacs-half-viewport place-emacs-half #:for-normal t)
+
 ( add-window-matcher '( ( WM_CLASS . "^Chromium/chromium$" ) )
 		     '( ( place-mode . full-or-half-viewport ) ))
 
 ( add-window-matcher '( ( WM_CLASS . "^Emacs/emacs$" ) )
-		     '( ( place-mode . half-viewport ) ))
+		     '( ( place-mode . emacs-half-viewport ) ))
 
 ( add-window-matcher '( ( WM_CLASS . "Firefox" )
 			( WM_WINDOW_ROLE . "browser") )
