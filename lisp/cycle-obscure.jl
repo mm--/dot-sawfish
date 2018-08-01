62357c8e36b4c9d9a2db656ac075d8c606ee4793
Josh Moller-Mara <j.mollermara@nyu.edu>
2018-08-01 10:23:46 +0800

    Cycle overlapping otherwise cycle all viewport windows

--- lisp/cycle-obscure.jl
+++ lisp/cycle-obscure.jl
@@ -5,21 +5,29 @@
 
 (define (get-intersecting-windows win)
   (let* ((win-rect (car (rectangles-from-windows (list win))))
-	 (other-rects (rectangles-from-windows (viewport-windows-except win) (lambda (x) x)))
-	 (intersecting-windows (filter windowp
-				       (mapcar (lambda (wr)
-						 (car (cddddr (rectangle-intersection wr win-rect))))
-					       other-rects))))
-    (cons win intersecting-windows)))
-
-(get-intersecting-windows (input-focus))
+	 (other-rects (rectangles-from-windows (viewport-windows-except win) (lambda (x) x))))
+    (filter windowp
+		 (mapcar (lambda (wr)
+			   (car (cddddr (rectangle-intersection wr win-rect))))
+			 other-rects))))
 
 (define-cycle-command-pair
   'cycle-obscure 'cycle-obscure-backwards
   (lambda (w)
-    (get-intersecting-windows w))
+    (cons w (get-intersecting-windows w)))
+  #:spec "%W")
+
+;; This doesn't exactly work because once it gets to a group of
+;; intersecting windows it'll get "stuck" in them.
+(define-cycle-command-pair
+  'cycle-obscure-dwim 'cycle-obscure-dwim-backwards
+  (lambda (w)
+    (let ((intersecting (get-intersecting-windows w)))
+      (if intersecting
+	  (cons w intersecting)
+	(filter-windows window-in-cycle-p))))
   #:spec "%W")
 
 (bind-keys global-keymap
-	   "W-TAB" 'cycle-obscure
+	   "W-TAB" 'cycle-obscure-dwim
 	   "W-C-TAB" 'cycle-windows)
